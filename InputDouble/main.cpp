c21086073e606d195811bd6056313d333fb370e5
Manny <captphasmanny@att.net>
2023-11-06 15:38:51 -0500

    Testing out expressions with inputs and outputs.

--- InputDouble/main.cpp
+++ InputDouble/main.cpp
@@ -7,8 +7,45 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+/*int inputNumber()
+{
+    int x;
+    std::cout << "Enter an integer: ";
+    std::cin >> x;
     return 0;
 }
+
+int doubleNumber()
+{
+    std::cout << "Double that number is: ";
+    return 0;
+}
+
+int main()
+{
+    int y = inputNumber();
+    int z = (y * 2);
+    std::cout << "Double that number is: " << z;
+}*/
+
+int main()
+{
+    int x{};
+    std::cout << "Enter an integer: ";
+    std::cin >> x;
+    int y{x*2};
+    std::cout << "Double " << x << " is: " << y << '\n';
+    std::cout << "Triple " << x << " is: " << x*3 << '\n';
+    
+    int a{};
+    std::cout << "Enter an integer: ";
+    std::cin >> a;
+    //std::cout <<'\n';
+    int b{};
+    std::cout << "Enter another integer: ";
+    std::cin >> b;
+    //std::cout <<'\n';
+    std::cout << a << " + " << b << " = " << a+b << '\n';
+    std::cout << a << " - " << b << " = " << a-b << '\n';
+}
+ 
